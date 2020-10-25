c6271a4f9f4c4b0e4dc6d6b8bcd04264f432390e
Pupilsch <821058047@qq.com>
2020-10-25 07:54:47 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
 if [ ! -d "go" ]; then
-	mkdir -p /home/xiyafan2/go/src
+	mkdir -p /home/chunhao3/go/src
 fi
 
 if [ ! -d "src" ]; then
-	mkdir -p /home/xiyafan2/go/pkg
+	mkdir -p /home/chunhao3/go/pkg
 fi
 
 export GOROOT=/usr/lib/golang
-export GOPATH=/home/xiyafan2/go
+export GOPATH=/home/chunhao3/go
 
 go get github.com/jinzhu/copier
 go get github.com/golang/protobuf/proto

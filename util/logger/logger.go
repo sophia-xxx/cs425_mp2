5f8654ccfee15e8b96f6eb7e428fac1247d7eb17
Hangar <HangaryLiu@gmail.com>
2020-10-26 02:35:04 +0800

    fix

--- util/logger/logger.go
+++ util/logger/logger.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
+	"time"
 )
 
 var (
@@ -19,18 +21,22 @@ var (
 
 	// ErrorLogger : logging for error messages
 	DebugLogger *log.Logger
+
+	LogFile *os.File
+
+	TimeFormat = "2006-01-02 15:04:05"
 )
 
 func init() {
-	file, err := os.OpenFile("logs.txt", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+	LogFile, err := os.OpenFile("logs.txt", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	InfoLogger = log.New(file, "[INFO]", log.Ldate|log.Ltime|log.Lshortfile)
-	WarningLogger = log.New(file, "[WARNING]", log.Ldate|log.Ltime|log.Lshortfile)
-	ErrorLogger = log.New(file, "[ERROR]", log.Ldate|log.Ltime|log.Lshortfile)
-	DebugLogger = log.New(file, "[DEBUG]", log.Ldate|log.Ltime|log.Lshortfile)
+	InfoLogger 		= log.New(LogFile, "[INFO]", log.Ldate|log.Ltime|log.Lshortfile)
+	WarningLogger 	= log.New(LogFile, "[WARNING]", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrorLogger 	= log.New(LogFile, "[ERROR]", log.Ldate|log.Ltime|log.Lshortfile)
+	DebugLogger 	= log.New(LogFile, "[DEBUG]", log.Ldate|log.Ltime|log.Lshortfile)
 }
 
 func PrintToConsole(args ...interface{}) {
@@ -40,28 +46,28 @@ func PrintToConsole(args ...interface{}) {
 func PrintInfo(args ...interface{}) {
 	InfoLogger.Println(args)
 
-	fmt.Print(green("[INFO] "))
+	fmt.Print(green("[INFO] " + "(" + strings.Split(time.Now().Format(TimeFormat), " ")[1] + ")"))
 	fmt.Print(green(fmt.Sprintln(args...)))
 }
 
 func PrintWarning(args ...interface{}) {
 	WarningLogger.Println(args)
 
-	fmt.Print(yellow("[WARN] "))
+	fmt.Print(yellow("[WARN] " + "(" + strings.Split(time.Now().Format(TimeFormat), " ")[1] + ")"))
 	fmt.Print(yellow(fmt.Sprintln(args...)))
 }
 
 func PrintError(args ...interface{}) {
 	ErrorLogger.Println(args)
 
-	fmt.Print(red("[ERROR]"))
+	fmt.Print(green("[ERROR] " + "(" + strings.Split(time.Now().Format(TimeFormat), " ")[1] + ")"))
 	fmt.Print(red(fmt.Sprintln(args...)))
 }
 
 func PrintDebug(args ...interface{}) {
 	ErrorLogger.Println(args)
 	if config.DebugMode {
-		fmt.Print(yellow("[DEBUG] "))
+		fmt.Print(yellow("[DEBUG]" + "(" + strings.Split(time.Now().Format(TimeFormat), " ")[1] + ")"))
 		fmt.Println(args...)
 	}
 }

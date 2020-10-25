249ba9812e211ae45d2692c38e666917b6e60052
Hangar <HangaryLiu@gmail.com>
2020-10-26 02:14:47 +0800

    fix

---
+++ util/logger/color.go
@@ -0,0 +1,26 @@
+package logger
+/*
+A package used to add color for log.
+Credit: codes come from https://toutiao.io/posts/2889gp/preview.
+*/
+import "fmt"
+
+const (
+	color_red = uint8(iota + 91)
+	color_green
+	color_yellow
+	color_blue
+)
+
+func red(s string) string {
+	return fmt.Sprintf("\x1b[%dm%s\x1b[0m", color_red, s)
+}
+func green(s string) string {
+	return fmt.Sprintf("\x1b[%dm%s\x1b[0m", color_green, s)
+}
+func yellow(s string) string {
+	return fmt.Sprintf("\x1b[%dm%s\x1b[0m", color_yellow, s)
+}
+func blue(s string) string {
+	return fmt.Sprintf("\x1b[%dm%s\x1b[0m", color_blue, s)
+}
--- util/logger/logger.go
+++ util/logger/logger.go
@@ -34,35 +34,34 @@ func init() {
 }
 
 func PrintToConsole(args ...interface{}) {
-	fmt.Println(args...)
+	fmt.Print(blue(fmt.Sprintln(args...)))
 }
 
 func PrintInfo(args ...interface{}) {
 	InfoLogger.Println(args)
 
-	fmt.Print("[INFO] ")
-	fmt.Println(args...)
+	fmt.Print(green("[INFO] "))
+	fmt.Print(green(fmt.Sprintln(args...)))
 }
 
 func PrintWarning(args ...interface{}) {
 	WarningLogger.Println(args)
 
-	fmt.Print("[WARNING] ")
-	fmt.Println(args...)
+	fmt.Print(yellow("[WARN] "))
+	fmt.Print(yellow(fmt.Sprintln(args...)))
 }
 
 func PrintError(args ...interface{}) {
 	ErrorLogger.Println(args)
 
-	fmt.Print("[ERROR] ")
-	fmt.Println(args...)
+	fmt.Print(red("[ERROR]"))
+	fmt.Print(red(fmt.Sprintln(args...)))
 }
 
 func PrintDebug(args ...interface{}) {
 	ErrorLogger.Println(args)
 	if config.DebugMode {
-		fmt.Print("[DEBUG] ")
+		fmt.Print(yellow("[DEBUG] "))
 		fmt.Println(args...)
 	}
-
 }
